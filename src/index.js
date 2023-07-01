e3545e591f1162a2cf1b4409707181a84cd1a2e6
Akash Jaiswal <akashjais929@gmail.com>
2023-07-01 15:19:46 +0530

    Completed UI

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'semantic-ui-css/semantic.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
+
   </React.StrictMode>
 );
 
